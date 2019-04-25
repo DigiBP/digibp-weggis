6377a9aa409b9552a4b17a95b3f18907b49f3c5d
Simon <i01002545@emea.endress.com>
2019-04-25 20:15:31 +0200

    change in bpmn + coding for google sheets

---
+++ Program code/setRequirements_stringify.js
@@ -0,0 +1,9 @@
+out = JSON.stringify({
+    "Name of Requestor": requestor,
+    "Department": department,
+    "Role": role,
+    "budget needed in CHF": budgetNeeded,
+    "Skill Requirements": skillRequirements,
+    "Years of Experience": yearsExperience,
+    "Educational Requirements": eduRequirements
+});
