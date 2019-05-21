9db464d214067c79c0ea99039b4246b08a49847c
Simon <i01002545@emea.endress.com>
2019-05-21 17:21:12 +0200

    jobopening

--- Program code/setRequirements_stringify.js
+++ Program code/setRequirements_stringify.js
@@ -1,9 +1,15 @@
 out = JSON.stringify({
+    "budgetAvailable": budgetAvailable,
+});
+
+
+out = JSON.stringify({
     "Name of Requestor": requestor,
     "Department": department,
-    "Role": role,
+    "Job": job,
     "budget needed in CHF": budgetNeeded,
     "Skill Requirements": skillRequirements,
     "Years of Experience": yearsExperience,
-    "Educational Requirements": eduRequirements
+    "Educational Requirements": eduRequirements,
+    "Description": description
 });
